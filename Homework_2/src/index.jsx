ecf3fe20cbab281e118f4a8e47a308ae2bdff5f1
Computermind11 <Computermind11@gmail.com>
2021-01-18 20:22:55 +0500

    Загрузил дз.

---
+++ Homework_2/src/index.jsx
@@ -0,0 +1,11 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import MessageField from "./messageField.jsx";
+
+
+ReactDOM.render(
+  <MessageField/>,
+  document.getElementById("container")
+);
